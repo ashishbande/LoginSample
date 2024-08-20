8adc9a6016bdec6a3126995aaaf06a7de090a26f
Ashish Bande <ashishba@zeta.tech>
2024-08-20 17:31:13 +0530

    my app first commit

---
+++ flutter_application_loginform/lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'src/myApp.dart';
+
+void main() {
+  runApp(myapp());
+}
---
+++ flutter_application_loginform/lib/src/myApp.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class myapp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: "Log me in",
+      home: Scaffold(
+        body: Text("Show a form here"),
+      ),
+    );
+  }
+}
